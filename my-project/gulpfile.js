52fa913708d1b1c14d689071d614ad3c02e5dabe
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-04-30 11:01:30 +0600

    gulp js

--- my-project/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-// const sass = require('gulp-sass');
-const sass = require('gulp-sass')(require('sass'));
-gulp.task('sample', ()=>{
-    return gulp.src('./src/scss/main.css')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist'));
-});
